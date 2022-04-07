2138e0e8a360ed25751d364c6b3d37e1b5c6c26e
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-04-07 11:52:21 +0800

    [FE] - Create Login UI

---
+++ frontend/src/components/LoginComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="submit">
+    <div class="mt-2 mb-3">
+      <label class="mb-3">Email Address</label>
+      <input type="text" class="form-control form-control-lg" placeholder="your@email.com">
+    </div>
+    <div class="mb-4">
+      <label class="mb-3">Password</label>
+      <input type="password" class="form-control form-control-lg" placeholder="Enter your Password">
+    </div>
+    <div class="d-grid mb-4">
+      <button type="submit" class="btn btn-primary btn-lg">Login</button>
+    </div>
+    <div class="d-grid mb-3 text-center">
+      <a href="#" class="text-muted">Forgot Password?</a>
+    </div>
+    <div class="d-grid mb-3 text-center">
+      <span class="text-muted">New here? <a href="#">Sign up</a></span>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponent'
+}
+</script>
+
+<style scoped>
+input, button {
+  border-radius: 0;
+}
+
+button {
+  background: #6c63ff;
+}
+</style>
