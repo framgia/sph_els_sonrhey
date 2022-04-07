a3e7a4aa2e2955db1645a5ad1fd533b3fbeadf93
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-04-07 13:43:02 +0800

    [FE] - Sign Up UI

---
+++ frontend/src/views/SignupView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main-wrapper">
+    <div class="sub-wrapper">
+      <div class="card shadow signup-card">
+        <div class="card-body" style="padding: 1.5rem;">
+          <div class="card-title border-bottom mb-4">
+            <h3>Signup</h3>
+            <div class="mb-4"></div>
+          </div>
+          <SignupComponent />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignupComponent from '../components/SignupComponent.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    SignupComponent
+  }
+}
+</script>
+
+<style scoped>
+@import '../assets/login.css';
+
+</style>
