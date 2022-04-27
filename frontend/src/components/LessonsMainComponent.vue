ceb79bdd5b6322473f391a0eff424fd13692218d
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-04-27 15:19:26 +0800

    Filter Status Lessons

--- frontend/src/components/LessonsMainComponent.vue
+++ frontend/src/components/LessonsMainComponent.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="mt-4">
     <div class="card shadow lesson-card">
-      <div class="card-body" style="padding: 1.5rem;">
+      <div class="card-body" style="padding: 1.5rem;">   
+        <div class="col-md-4 mb-3">
+          <h5>Filter by Status</h5>
+          <select class="form-select" aria-label="Default select example" @change="selectStatus">
+            <option :value="completed">Completed</option>
+            <option :value="inProgress">In Progress</option>
+            <option :value="defaultStatus">Unused</option>
+            <option :value="displayAll" selected>All</option>
+          </select>
+        </div>
         <div class="lesson-list">
           <LessonsListComponent v-for="category in categories" :key="category.category_id" :category="category" />
         </div>
@@ -14,6 +23,8 @@
 import LessonsListComponent from '../components/LessonsListComponent.vue'
 import getCategory from '../composables/getCategory'
 import commonService from '../composables/commonService'
+import quizStatuses from '../composables/quizStatuses'
+import { ref, watchEffect } from 'vue'
 
 export default {
   name: 'LessonsMainComponent',
@@ -21,11 +32,33 @@ export default {
     LessonsListComponent
   },
   setup() {
-    const { getCategoryWithQuestions, categoriesWithQuestion } = getCategory()
+    const { getUserAndToken } = commonService()
+    const { getCategoryWithQuestions, categoriesWithQuestion, getCategoriesUsed, categoriesUsed, isLoaded, filterUsedCategory, filterUnusedCategory } = getCategory()
+    const { defaultStatus, inProgress, completed, displayAll } = quizStatuses()
+    const userId = JSON.parse(getUserAndToken('user')).user_id
     const getCategories = getCategoryWithQuestions()
-    const categories = categoriesWithQuestion
-    
-    return { categories }
+    const getCatUsed = getCategoriesUsed(userId)
+    const categories = ref()
+
+    watchEffect(() => {
+      if (isLoaded.value) {
+        categories.value = categoriesWithQuestion.value
+      }
+    })
+
+    const selectStatus = (e) => {
+      if (e.target.value === defaultStatus) {
+        const categoryList = categoriesUsed.value
+        categories.value = filterUnusedCategory(categoriesWithQuestion.value, categoryList)
+      } else if (e.target.value === displayAll) {
+        categories.value = categoriesWithQuestion.value
+      } else {
+        const categoryList = categoriesUsed.value.filter(q => q.status.code === e.target.value)
+        categories.value = filterUsedCategory(categoriesWithQuestion.value, categoryList)
+      }
+    }
+
+    return { categories, defaultStatus, inProgress, completed, selectStatus, displayAll }
   }
 }
 </script>
--- frontend/src/composables/quizStatuses.js
+++ frontend/src/composables/quizStatuses.js
@@ -2,8 +2,9 @@ const quizStatuses = () => {
   const completed = 'CMP'
   const inProgress = 'INP'
   const defaultStatus = 'DEF'
+  const displayAll = 'ALL'
 
-  return { completed, inProgress, defaultStatus }
+  return { completed, inProgress, defaultStatus, displayAll }
 }
 
 export default quizStatuses
