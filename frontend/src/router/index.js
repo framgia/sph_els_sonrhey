4b0c5e63e99bb42d4c03f7dacbb92c99dba01308
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-04-28 16:57:01 +0800

    Navigation Guard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,73 +12,106 @@ import AccountCreatedView from '../views/AccountCreatedView.vue'
 import CategoryViewQuestions from '../views/CategoryViewQuestions.vue'
 import ProfileVisitView from '../views/ProfileVisitView.vue'
 import MyActivityLogView from '../views/MyActivityLogView.vue'
+import commonService from '../composables/commonService'
+
+const { getUserAndToken } = commonService()
+
+const checkAuth = () => {
+  if(getUserAndToken('token') == null){
+    return { path: '/' }
+  }
+}
+
+const redirectIfAuthenticated = (to, from) => {
+  if(getUserAndToken('token')){
+    return { path: '/dashboard' }
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    beforeEnter: [ redirectIfAuthenticated ]
   },
   {
     path: '/signup',
     name: 'SignUp',
-    component: SignupView
+    component: SignupView,
+    beforeEnter: [ redirectIfAuthenticated ]
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: DashboardView
+    component: DashboardView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/profile',
     name: 'Profile',
-    component: ProfileView
+    component: ProfileView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/users',
     name: 'Users',
-    component: UsersView
+    component: UsersView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/category',
     name: 'Category',
-    component: CategoryView
+    component: CategoryView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/questions',
     name: 'Questions',
-    component: QuestionsView
+    component: QuestionsView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/lessons',
     name: 'Lessons',
-    component: LessonsView
+    component: LessonsView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/answer',
     name: 'Answer',
-    component: AnswersView
+    component: AnswersView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/account-created',
     name: 'AccountCreated',
-    component: AccountCreatedView
+    component: AccountCreatedView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/category/:name',
     name: 'CategoryViewQuestions',
-    component: CategoryViewQuestions
+    component: CategoryViewQuestions,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/profile-visit',
     name: 'ProfileVisit',
-    component: ProfileVisitView
+    component: ProfileVisitView,
+    beforeEnter: [ checkAuth ]
   },
   {
     path: '/my-activity-log',
     name: 'MyActivityLog',
-    component: MyActivityLogView
-  }
+    component: MyActivityLogView,
+    beforeEnter: [ checkAuth ]
+  },
+  { 
+    path: '/:pathMatch(.*)*', 
+    name: 'NotFound', 
+    beforeEnter: [ checkAuth, redirectIfAuthenticated ]
+  },
 ]
 
 const router = createRouter({
